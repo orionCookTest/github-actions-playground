f188a764a322f91ca3e613a21472918babaa2685
Orion Cook <orion.cook@billgo.com>
2021-02-15 21:30:40 -0700

    dummy dropwizard server accessible - take two with the config files

---
+++ src/main/java/com/spacebug/restjava/RestJava.java
@@ -0,0 +1,28 @@
+package com.spacebug.restjava;
+
+import com.spacebug.restjava.health.RestJavaHealthCheck;
+import com.spacebug.restjava.resource.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class RestJava extends Application<RestJavaConfiguration> {
+
+
+    public static void main(String[] args) throws Exception {
+        new RestJava().run(args);
+    }
+
+    @Override
+    public void run(RestJavaConfiguration configuration, Environment environment) throws Exception {
+        registerResources(environment);
+        registerHealthChecks(environment);
+    }
+
+    private void registerResources(Environment environment) {
+        environment.jersey().register(HelloWorldResource.class);
+    }
+
+    private void registerHealthChecks(Environment environment) {
+        environment.healthChecks().register("restjava", new RestJavaHealthCheck());
+    }
+}
---
+++ src/main/java/com/spacebug/restjava/RestJavaConfiguration.java
@@ -0,0 +1,6 @@
+package com.spacebug.restjava;
+
+import io.dropwizard.Configuration;
+
+public class RestJavaConfiguration extends Configuration {
+}
